941da8f53b28dc2a11b3489c5904102ef2d4761a
Marek Rydlewski <marek.rydlewski@outlook.com>
2017-10-12 00:21:30 +0200

    Fix loops

--- AlgBattle/RandomGenerator.cs
+++ AlgBattle/RandomGenerator.cs
@@ -10,7 +10,7 @@ namespace AlgBattle
 
         public static void ShuffleList<T>(IList<T> toShuffle)
         {
-            for (int i = toShuffle.Count - 1; i >= 0; ++i)
+            for (int i = toShuffle.Count - 1; i >= 0; --i)
             {
                 var j = Rnd.Next(0, i);
                 toShuffle.Swap(i, j);
@@ -19,7 +19,7 @@ namespace AlgBattle
 
         public static void ShuffleArray<T>(T[] toShuffle)
         {
-            for (int i = toShuffle.Length - 1; i >= 0; ++i)
+            for (int i = toShuffle.Length - 1; i >= 0; --i)
             {
                 var j = Rnd.Next(0, i);
                 T temp = toShuffle[i];
