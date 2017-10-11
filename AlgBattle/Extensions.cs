5ce5695dae169b9bea83a80ce3d8fc4536d44ca5
Marek Rydlewski <marek.rydlewski@outlook.com>
2017-10-12 00:18:56 +0200

    Shuffle arrays

---
+++ AlgBattle/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgBattle
+{
+    public static class IListExtensions
+    {
+        public static void Swap<T>(
+            this IList<T> list,
+            int firstIndex,
+            int secondIndex
+        )
+        {
+            if (firstIndex == secondIndex)
+            {
+                return;
+            }
+            T temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+        }
+    }
+}
---
+++ AlgBattle/RandomGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgBattle
+{
+    public class RandomGenerator
+    {
+        public static Random Rnd { get; set; } = new Random();
+
+        public static void ShuffleList<T>(IList<T> toShuffle)
+        {
+            for (int i = toShuffle.Count - 1; i >= 0; ++i)
+            {
+                var j = Rnd.Next(0, i);
+                toShuffle.Swap(i, j);
+            }
+        }
+
+        public static void ShuffleArray<T>(T[] toShuffle)
+        {
+            for (int i = toShuffle.Length - 1; i >= 0; ++i)
+            {
+                var j = Rnd.Next(0, i);
+                T temp = toShuffle[i];
+                toShuffle[i] = toShuffle[j];
+                toShuffle[i] = temp;
+            }
+        }
+    }
+}
