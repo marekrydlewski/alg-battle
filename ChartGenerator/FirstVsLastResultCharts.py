01debebd8d767ab6867f7e9bcd92c23a15ab691d
Sebastian.Pawlak <Sebastian.Pawlak@IT066NB>
2017-11-15 00:15:05 +0100

    better chart

--- ChartGenerator/FirstVsLastResultCharts.py
+++ ChartGenerator/FirstVsLastResultCharts.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 
-files = ['../AlgBattle/greedyfirstVsLastsResult_bur26f.csv',
+files = ['../AlgBattle/steepest_firstVsLastsResult_chr12b.csv',
          '../AlgBattle/greedyfirstVsLastsResult_chr12b.csv']
 
 
@@ -24,13 +24,30 @@ def convert_data(data):
     return data_to_chart_x, data_to_chart_y
 
 
-def create_chart(data, filename):
-    data_x,data_y = convert_data(data)
-
-    plt.scatter(data_x, data_y, s=np.ones(len(data_x)), c=np.ones(len(data_x)), alpha=0.5)
+def create_chart(greedy_data, steepest_data, filename):
+    g_data_x, g_data_y = convert_data(greedy_data)
+    s_data_x, s_data_y = convert_data(steepest_data)
+    fig, ax = plt.subplots()
+    plt.xlabel('jakość początkowa')
+    plt.ylabel('jakość końcowa')
+    plt.scatter(g_data_x, g_data_y,
+                s=np.ones(len(g_data_x)) * 2,
+                c='red',
+                label='greedy',
+                alpha=0.9,
+                edgecolors='none')
+    plt.scatter(s_data_x, s_data_y,
+                s=np.ones(len(g_data_x)) * 2,
+                c='blue',
+                label='steepest',
+                alpha=0.9,
+                edgecolors='none')
+    ax.legend()
+    ax.grid(True)
     plt.savefig(filename + ".svg")
+    plt.savefig(filename + ".png")
 
 
-
-data = read_file(files[0])
-create_chart(data, "first_vs_last_chart")
+steepest_data = read_file(files[0])
+greedy_data = read_file(files[1])
+create_chart(greedy_data, steepest_data, "first_vs_last_chart")
