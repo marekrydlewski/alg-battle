d39cf28e5de55e30dbbec25deb9073e229ec8dc4
Marek Rydlewski <marek.rydlewski@outlook.com>
2017-11-30 12:35:13 +0100

    Add annealing skeleton

---
+++ AlgBattle/Solvers/QapAnnealingSolver.cs
@@ -0,0 +1,24 @@
+﻿using AlgBattle.DataReaders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgBattle.Solvers
+{
+    public class QapAnnealingSolver : QapSolver
+    {
+        public QapAnnealingSolver(QapData data) : base(data)
+        {
+        }
+
+        public override int[] GetSolution()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetSwapCounter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
