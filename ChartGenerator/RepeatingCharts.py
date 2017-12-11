09af5f040acbd211413b31519c8725a6f05a10cc
Sebastian.Pawlak <Sebastian.Pawlak@IT066NB>
2017-12-11 21:35:57 +0100

    repeating charts

--- ChartGenerator/RepeatingCharts.py
+++ ChartGenerator/RepeatingCharts.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 
-files = ["esc16j", "chr12a", "tai15b", "tai20b", "els19", "esc16a", "tai15a"]
+files = ["tai15a", "tai20b", "chr12a"]
 
 
 def read_file(filename):
@@ -20,7 +20,7 @@ def convert_data(data):
     data_to_chart_best = []
     i = 0
     for x in data:
-        if i>150:
+        if i > 150:
             break
         i += 1
         data_to_chart_x.append(i)
@@ -29,9 +29,11 @@ def convert_data(data):
     return data_to_chart_x, data_to_chart_average, data_to_chart_best
 
 
-def create_chart(greedy_data, steepest_data, filename):
+def create_chart(greedy_data, steepest_data, annealing_data, tabu_data, filename):
     g_data_x, g_data_av, g_data_b = convert_data(greedy_data)
     s_data_x, s_data_av, s_data_b = convert_data(steepest_data)
+    a_data_x, a_data_av, a_data_b = convert_data(annealing_data)
+    t_data_x, t_data_av, t_data_b = convert_data(tabu_data)
     fig, ax = plt.subplots()
     plt.ylabel('jakość')
     plt.xlabel('liczba powtórzeń')
@@ -63,14 +65,44 @@ def create_chart(greedy_data, steepest_data, filename):
                 label='steepest średni',
                 alpha=0.9,
                 edgecolors='none')
+    plt.scatter(a_data_x, a_data_b,
+                s=np.ones(len(g_data_x)) * 6,
+                c='brown',
+                marker="o",
+                label='annealing najlepszy',
+                alpha=0.9,
+                edgecolors='none')
+    plt.scatter(a_data_x, a_data_av,
+                s=np.ones(len(g_data_x)) * 6,
+                c='gray',
+                marker="o",
+                label='annealing średni',
+                alpha=0.9,
+                edgecolors='none')
+    plt.scatter(t_data_x, t_data_b,
+                s=np.ones(len(g_data_x)) * 6,
+                c='pink',
+                marker="o",
+                label='tabu najlepszy',
+                alpha=0.9,
+                edgecolors='none')
+    plt.scatter(t_data_x, t_data_av,
+                s=np.ones(len(g_data_x)) * 6,
+                c='violet',
+                marker="o",
+                label='tabu średni',
+                alpha=0.9,
+                edgecolors='none')
     ax.legend()
     ax.grid(True)
-   # plt.savefig(filename + ".svg")
+    # plt.savefig(filename + ".svg")
     plt.savefig(filename + ".png")
     plt.savefig(filename + ".pdf")
 
 
 for file in files:
-    steepest_data = read_file('../AlgBattle/steepest_repeating_' + file + '.csv')
-    greedy_data = read_file('../AlgBattle/greedy_repeating_' + file + '.csv')
-    create_chart(greedy_data, steepest_data, file)
+    steepest_data = read_file('../AlgBattle/repeating_steepest_' + file + '.csv')
+    greedy_data = read_file('../AlgBattle/repeating_greedy_' + file + '.csv')
+    annealing_data = read_file('../AlgBattle/repeating_annealing_' + file + '.csv')
+    tabu_data = read_file('../AlgBattle/repeating_tabu_' + file + '.csv')
+    create_chart(greedy_data, steepest_data, annealing_data, tabu_data, file)
